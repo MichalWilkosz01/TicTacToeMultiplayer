6245323b5fbcd9048811992b79fdf9134f74e0f9
Michal <mw7883475@gmail.com>
2024-08-05 19:16:02 +0200

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string serverIp = "127.0.0.1";
+        int port = 13000;
+        TcpClient? tcppClient;
+        NetworkStream? stream;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            tcppClient = new TcpClient(serverIp, port);
+            stream = tcppClient.GetStream();
+        }
+
+        private void btnSendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            if (tcppClient is null || stream is null)
+            {
+                MessageBox.Show("Najpierw należy połączyć się z serwerem!");
+                return;
+            }
+                
+            string message = txtMessage.Text;
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+
+        }
+
+        private void btnDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            stream?.Close();
+            tcppClient?.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        TcpListener? server = null;
+        try
+        {
+            // Adres IP i port
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            int port = 13000;
+
+            // Utwórz serwer na danym adresie i porcie
+            server = new TcpListener(localAddr, port);
+
+            // Start serwera
+            server.Start();
+
+            // Oczekiwanie na połączenie klienta
+            Console.WriteLine("Serwer uruchomiony. Oczekiwanie na połączenie...");
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Połączono z klientem!");
+
+                ClientHandler clientHandler = new ClientHandler(client);
+                clientHandler.HandleClient();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        finally
+        {
+            server?.Stop();
+        }
+    }
+}
+
+class ClientHandler
+{
+    private TcpClient client;
+
+    public ClientHandler(TcpClient client)
+    {
+        this.client = client;
+    }
+
+    public void HandleClient()
+    {
+        // Obsługa połączenia z klientem
+        NetworkStream stream = client.GetStream();
+        byte[] data = new byte[256];
+        string? responseData = null;
+
+        int bytes = stream.Read(data, 0, data.Length);
+        responseData = Encoding.ASCII.GetString(data, 0, bytes);
+        Console.WriteLine("Otrzymano: {0}", responseData);
+
+        // Odpowiedź do klienta
+        byte[] msg = Encoding.ASCII.GetBytes("Otrzymano twoją wiadomość.");
+        stream.Write(msg, 0, msg.Length);
+
+        // Zamknięcie połączenia
+        client.Close();
+    }
+}
